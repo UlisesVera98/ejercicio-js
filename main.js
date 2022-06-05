a0ece4f04826a9bf3f22cc1ef56a2b0d5ca945eb
Ulises Vera <ulisesveraaa9814@gmail.com>
2022-06-05 18:33:34 -0300

    ejercicio javascript

---
+++ main.js
@@ -0,0 +1,29 @@
+let clima = prompt("Ingrese un día")
+switch(clima) {
+    case "lunes":
+     alert ("El lunes estará despejado.");
+     break;
+   case "martes":
+     alert("El martes estará soloeado.");
+     break;
+   case "miercoles":
+    alert ("El miercoles estará con viento.");
+     break;
+   case "jueves":
+     alert ("El jueves estará parcialmente soleado.");
+     break;
+   case "viernes":
+    alert ("El viernes estará parcialmente nublado con probabilidad de lluvia.");
+     break;
+   case "sabado":
+    alert("El sábado estará nublado.");
+     break;
+   case "domingo":
+      alert ("El domingo estará lluvioso.");
+      break;
+     
+   default:
+     alert("El día asignado no es correcto");
+     
+     
+   }
